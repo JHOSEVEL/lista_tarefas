b09b606695fe371f16f2831490c421604e0f6b57
josevel silva <josevelmma5@gmail.com>
2023-12-13 17:09:59 -0300

    projeto lista de tarefa

---
+++ list.js
@@ -0,0 +1,36 @@
+//esconder formulario
+$(document).ready(function(){
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const novaTarefa = $('#texto').val();
+
+        const list_tarefas = $(`<li> ${novaTarefa}</li>`);
+        $(list_tarefas).appendTo('ol');
+        $('#texto').val('');
+
+    })
+    $('ol').on('click', 'li', function(){
+        $(this).toggle('strike-through');
+        //$(this).toggle('line-through');
+    })
+
+ 
+ })
+ 
+
+
+
+/*
+function adicionar(){
+    let text = document.getElementById('texto').value;
+    let list = document.getElementById('lista').innerHTML;
+
+    list +=  '<a> '+ text+' </a>'
+
+    document.getElementById('lista').innerHTML = list;
+
+
+    
+
+}*/
